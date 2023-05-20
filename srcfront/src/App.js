347e9c2b38693362b8fc03d76053cd73ee053ad9
nositelshtanov <groshev523000@gmail.com>
2023-05-20 16:34:04 +0300

    Сделана лента, форма авторизации, роутинг, механизм авторизации в контексте с багом

--- srcfront/src/App.js
+++ srcfront/src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter} from "react-router-dom";
+import "./main.css"
+import "normalize.css"
+import RouterList from "./components/RouterList/RouterList";
+import { AuthContext } from "./context";
+import { useState } from "react";
+
 function App() {
-  return (
-    <div className="App"></div>
-  );
+    const [isAuth, setIsAuth] = useState(false);
+
+    return (
+        <AuthContext.Provider value={{isAuth, setIsAuth}}>
+            <BrowserRouter>
+                <div className="App">
+                    <RouterList />
+                </div>
+            </BrowserRouter>
+        </AuthContext.Provider>
+    );
 }
 
 export default App;
