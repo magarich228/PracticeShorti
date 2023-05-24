5fa5f372d59ef1347951505cae7c2ceed862242c
nositelshtanov <groshev523000@gmail.com>
2023-05-24 12:20:15 +0300

    Working on video add page

--- src/Web/src/pages/Subscribes.jsx
+++ src/Web/src/pages/Subscribes.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
-import NavBar from '../components/NavBar/NavBar'
+import { useState } from 'react';
+import VideoPlayer from '../components/VideoPlayer/VideoPlayer';
+import NavBar from '../components/NavBar/NavBar';
+import VideoData from '../components/VideoData/VideoData';
+import Search from '../components/Search/Search';
 
 export default function Subscribes() {
+    const videos = ["/short4.mp4", "/short2.mp4", "/short3.mp4", "/short1.mp4", "/video1.mp4", "/video2.mp4", "/video3.mp4"];
+    const [curVideo, setCurVideo] = useState(0);
+
     return (
-        <div>
-            <h1>Subscribes</h1>
-            <NavBar />
+        <div className="SubsPage">
+            <main className="PlayerContainer">
+                <VideoPlayer 
+                    videos={videos} 
+                    curVideo={curVideo} 
+                    setCurVideo={(curIndex) => setCurVideo(curIndex)} 
+                    like={true}
+                >
+                    <Search />
+                </VideoPlayer>
+            </main>
+            <aside className="SubsAside">
+                <nav className="SubsNav">
+                    <NavBar />
+                </nav>
+                <div className="AsideContent">
+                    <VideoData curVideo={videos[curVideo]} />
+                </div>
+            </aside>
         </div>
     )
 }
