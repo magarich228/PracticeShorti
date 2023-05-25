e3bc0e05874124a4f204c35ffa4406d21d9981bf
magarich228 <kirillgroshev52@gmail.com>
2023-05-25 16:04:05 +0300

    Добавлен контекст БД активностей.

---
+++ src/Services/Activities/Shorti.Activities.Api/Data/ActivitiesContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shorti.Activities.Api.Data.Models;
+
+namespace Shorti.Activities.Api.Data
+{
+    public class ActivitiesContext : DbContext
+    {
+        public DbSet<Subscription> Subscriptions { get; set; } = null!;
+        public DbSet<LikeReaction> Likes { get; set; } = null!;
+
+        public ActivitiesContext(DbContextOptions<ActivitiesContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- src/Services/Activities/Shorti.Activities.Api/Program.cs
+++ src/Services/Activities/Shorti.Activities.Api/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Shorti.Activities.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("ActivitiesContext") ??
+    throw new InvalidOperationException("Connection string 'ActivitiesContext' not found.");
+
+builder.Services.AddDbContext<ActivitiesContext>(options =>
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 
